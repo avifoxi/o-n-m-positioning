ba214b834502eaed89b08a86a9eae3abe17098f4
AviFoxi <frosenox@gmail.com>
2015-08-30 23:58:11 -0400

    and it basically does its job

--- sizing.js
+++ sizing.js
@@ -1,50 +1,17 @@
 $(document).ready(function() {
 	
-	var header = $('#header'),
-		spacer = $('#spacer'),
-		cols = $('.col')
-		textToCenter = $('p.center-text');
+	var $textParent = $('.text-parent'),
+		$text = $('.center-text');
 
-	normalizePage();
+	centerText();
 	$( window ).resize( function(){
-		normalizePage();
+		centerText();
 	})
 
-	function normalizePage(){
-		padHeader();
-		normalizeCols();
-	}
-
-	function padHeader(){
-		var headerPad = header.height();
-		spacer.height( headerPad );
-	}
-
-	function normalizeCols(){
-		// this is not my ideal solution -- 
-		// there are bugs with multiple resizes
-		// definitely something to improve
-		textToCenter.css({
-			marginTop: 0
-		});
-		var max = 0,
-			test = 0,
-			textHeight = textToCenter.height(),
-			marginTop = 0;
-		
-		cols.each(function( index, col ){
-			test = $( col ).height();
-			if ( test > max ){
-				max = test;
-			}
-		});
-
-		textToCenter.parent().height( max );
-		marginTop = ( max - textHeight )  / 2 ;
-		
-		textToCenter.css({
-			marginTop: marginTop
-		});
+	function centerText(){
+		var colHeight = $textParent.height();
+		var textHeight = $text.height();
+		$text.css('top', colHeight / 2 - textHeight / 2);
 	}
 
 
